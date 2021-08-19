2809629e4333e77133e8e83fe2f6581cfc5becdb
maya <maya.salcedo07@gmail.com>
2021-08-19 20:59:46 +0300

    installed styled-components and added GlobalStyle in elements folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import HomeScreen from './screens/HomeScreen';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import GlobalStyle from './elements/GlobalStyle';
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Header></Header>
       <HomeScreen />
       <Footer />
