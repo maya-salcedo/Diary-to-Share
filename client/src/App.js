e1acf0fe486b84be4ea5fde23ff8016da9d3d6b2
maya <maya.salcedo07@gmail.com>
2021-08-20 12:53:47 +0300

    installed react-router-dom, added route to diary screen component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import HomeScreen from './screens/HomeScreen';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import GlobalStyle from './elements/GlobalStyle';
+import DiaryScreen from './screens/DiaryScreen';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <GlobalStyle />
       <Header></Header>
-      <HomeScreen />
+      <main>
+        <Route path="/diary" component={DiaryScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+      </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
