8761fc3382b36af311c102e967298708b8b7ef0f
maya <maya.salcedo07@gmail.com>
2021-08-23 20:01:41 +0300

    created a store

---
+++ client/src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+
+const initialState = {};
+
+const reducer = (state, action) => {
+  return { diaries: x };
+};
+
+const store = createStore(reducer, initialState);
+
+export default store;
