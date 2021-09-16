229284887ec4c25dae0d3d90f94234390b87fc1c
maya <maya.salcedo07@gmail.com>
2021-09-16 21:47:12 +0300

    added toggle button for dark mode

--- client/src/container/header.jsx
+++ client/src/container/header.jsx
@@ -15,10 +15,21 @@ export function HeaderContainer() {
           alt="Diary-to-Share"
         />
         <Header.TitleLink>Diary-to-Share</Header.TitleLink>
-        <Header.Column>
+        {/* <Header.Column>
           <Header.ButtonToggle onClick={toggleTheme} className={`${theme}`}>
             Switch Theme
           </Header.ButtonToggle>
+        </Header.Column> */}
+        <Header.Column>
+          <div class="switch-holder">
+            <div class="switch-label">
+              <span>Dark mode</span>
+            </div>
+            <div class="switch-toggle">
+              <input type="checkbox" id="bluetooth" />
+              <label for="bluetooth"></label>
+            </div>
+          </div>
         </Header.Column>
       </Header.Row>
       <Header.Row>
